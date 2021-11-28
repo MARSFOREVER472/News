9a8857915f4ba9c9abbdbf39c6892f66c84321d0
Marcelo Lam <marcelo.lam@alumnos.ucn.cl>
2021-11-28 17:34:37 -0300

    feat(model): add room database

---
+++ app/src/main/java/cl/ucn/disc/dsm/mlam/news/model/NewsRoomDatabase.java
@@ -0,0 +1,31 @@
+package cl.ucn.disc.dsm.mlam.news.model;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Article.class}, version = 1, exportSchema = false)
+public abstract class NewsRoomDatabase extends RoomDatabase {
+
+    public abstract NewsDao getNewsDao();
+
+    // Singleton
+    private static NewsRoomDatabase INSTANCE;
+    public static NewsRoomDatabase getDatabase(Context context){
+        if(INSTANCE == null){
+            synchronized (NewsRoomDatabase.class){
+
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NewsRoomDatabase.class, "NewsDB")
+                            .fallbackToDestructiveMigration()
+                            .build();
+
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
