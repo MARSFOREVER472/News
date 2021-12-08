3268f02a745780e2f91432b245f2d69c058e28f2
Marcelo Lam <marcelo.lam@alumnos.ucn.cl>
2021-12-08 12:36:13 -0300

    feat(news): implement main activity

--- app/src/main/java/cl/ucn/disc/dsm/mlam/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/mlam/news/MainActivity.java
@@ -1,14 +1,45 @@
 package cl.ucn.disc.dsm.mlam.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.mlam.news.model.Article;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NewsViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        NewsAdapter adapter = new NewsAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // ViewModel
+        viewModel = new ViewModelProvider(this).get(NewsViewModel.class);
+        viewModel.getAllNews().observe(this, new Observer<List<Article>>() {
+            @Override
+            public void onChanged(List<Article> articles) {
+                adapter.setNews(articles);
+            }
+        });
+    }
+
+    public void refresh(View view) {
+
+        viewModel.refresh();
     }
 }
